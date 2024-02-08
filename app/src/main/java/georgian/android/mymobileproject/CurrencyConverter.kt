b2b864e003902c4fabe8965e600cafe6e36518b7
adedoyinomiyale <200534745@student.georgianc.on.ca>
2024-02-08 17:48:26 -0500

    uploaded codes

---
+++ app/src/main/java/georgian/android/mymobileproject/CurrencyConverter.kt
@@ -0,0 +1,11 @@
+package georgian.android.mymobileproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CurrencyConverter : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_currency_converter)
+    }
+}
